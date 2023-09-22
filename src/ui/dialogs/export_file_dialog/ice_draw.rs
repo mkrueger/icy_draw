0da46634d618a059c2162e5f709a97ab4b6b8dff
Mike Krüger <mkrueger@posteo.de>
2023-09-22 22:05:31 +0200

    Implemented the most important feature.
    IceDraw (not IcyDraw!) data format compression can now be toggled.

--- src/ui/dialogs/export_file_dialog/ice_draw.rs
+++ src/ui/dialogs/export_file_dialog/ice_draw.rs
@@ -1,8 +1,16 @@
-use eframe::egui::Ui;
+use eframe::egui::{self, Ui};
+use i18n_embed_fl::fl;
 use icy_engine::SaveOptions;
 
-use super::ascii;
-
 pub fn create_settings_page(ui: &mut Ui, options: &mut SaveOptions) {
-    ascii::create_settings_page(ui, options);
+    ui.vertical(|ui| {
+        ui.checkbox(
+            &mut options.compress,
+            fl!(crate::LANGUAGE_LOADER, "export-compression-label"),
+        );
+        ui.add(egui::Checkbox::new(
+            &mut options.save_sauce,
+            fl!(crate::LANGUAGE_LOADER, "export-save-sauce-label"),
+        ));
+    });
 }
--- src/ui/dialogs/export_file_dialog/xbin.rs
+++ src/ui/dialogs/export_file_dialog/xbin.rs
@@ -3,12 +3,14 @@ use i18n_embed_fl::fl;
 use icy_engine::SaveOptions;
 
 pub fn create_settings_page(ui: &mut Ui, options: &mut SaveOptions) {
-    ui.checkbox(&mut options.compress, fl!(
-        crate::LANGUAGE_LOADER,
-        "export-compression-label"
-    ));
-    ui.add(egui::Checkbox::new(
-        &mut options.save_sauce,
-        fl!(crate::LANGUAGE_LOADER, "export-save-sauce-label"),
-    ));
+    ui.vertical(|ui| {
+        ui.checkbox(
+            &mut options.compress,
+            fl!(crate::LANGUAGE_LOADER, "export-compression-label"),
+        );
+        ui.add(egui::Checkbox::new(
+            &mut options.save_sauce,
+            fl!(crate::LANGUAGE_LOADER, "export-save-sauce-label"),
+        ));
+    });
 }
